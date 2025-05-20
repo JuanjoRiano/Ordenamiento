46b30e4a911ae87b29cb862a36fcf978cc8bed4b
JuanjoRiano <juansamsumg07@gmail.com>
2025-05-20 09:31:32 -0500

    Update Main.java

--- ejercicio2/Main.java
+++ ejercicio2/Main.java
@@ -1,171 +1,220 @@
-package ejercicio1;
+package ejercicio2;
 
 import java.util.Random;
-import java.util.LinkedList;
-
-class Venta {
-    int codigoProducto;
-    int cantidadVendida;
-    double precioVenta;
-    
-    public Venta(int codigoProducto, int cantidadVendida, double precioVenta) {
-        this.codigoProducto = codigoProducto;
-        this.cantidadVendida = cantidadVendida;
-        this.precioVenta = precioVenta;
-    }
-}
 
 public class Main {
+    // Constantes
+    private static final int MAX_EMPLEADOS = 10;
+    private static final int SUELDO_BASICO = 0;
+    private static final int DEDUCCIONES = 1;
+    private static final int NETO_PAGAR = 2;
     
-    static class ListaVentas {
-        LinkedList<Venta> ventas;
-        
-        public ListaVentas() {
-            this.ventas = new LinkedList<>();
-        }
-        
-        /**
-         * Añade una nueva venta a la lista
-         */
-        public void agregarVenta(int codigoProducto, int cantidadVendida, double precioVenta) {
-            Venta nuevaVenta = new Venta(codigoProducto, cantidadVendida, precioVenta);
-            ventas.add(nuevaVenta);
-        }
+    // Variables
+    private int[] identificaciones;
+    private String[] nombres;
+    private double[][] datosNomina;
+    private int cantidadEmpleados;
+    
+    // Constructor
+    public Main() {
+        identificaciones = new int[MAX_EMPLEADOS];
+        nombres = new String[MAX_EMPLEADOS];
+        datosNomina = new double[MAX_EMPLEADOS][3]; 
+        cantidadEmpleados = 0;
     }
     
-    /**
-     * Pobla la lista con datos aleatorios
-     * @param lista Lista a poblar
-     */
-    public static void poblarDatos(ListaVentas lista) {
-        Random rand = new Random();
-        // Generar un conjunto de códigos de productos (algunos se repetirán)
-        int[] codigosProductos = new int[20];
-        for (int i = 0; i < codigosProductos.length; i++) {
-            // Generar códigos de 4 dígitos (entre 1000 y 9999)
-            codigosProductos[i] = 1000 + rand.nextInt(9000);
-        }
+    public void poblarDatos() {
+        Random random = new Random();
+        String[] nombresDisponibles = {"Juan", "Ana", "Carlos", "María", "Pedro", "Laura", "Damaris", 
+                                      "Albita", "Pedrito", "Juanita", "Pachita", "Ricardo"};
         
-        // Generar 50 ventas aleatorias
-        for (int i = 0; i < 50; i++) {
-            int codigoProducto = codigosProductos[rand.nextInt(codigosProductos.length)];
-            int cantidadVendida = 1 + rand.nextInt(20); // Entre 1 y 20 unidades
-            double precioVenta = 10000 + rand.nextDouble() * 90000; // Entre 10000 y 100000
-            precioVenta = Math.round(precioVenta * 100.0) / 100.0; // Redondear a 2 decimales
+        cantidadEmpleados = MAX_EMPLEADOS;
+        
+        for (int i = 0; i < cantidadEmpleados; i++) {
+
+        	identificaciones[i] = 1000 + random.nextInt(9000);
             
-            lista.agregarVenta(codigoProducto, cantidadVendida, precioVenta);
-        }
-    }
-    
-    /**
-     * Muestra los datos de las ventas por consola
-     * @param lista Lista a mostrar
-     */
-    public static void mostrarDatos(ListaVentas lista) {
-        if (lista.ventas.isEmpty()) {
-            System.out.println("La lista está vacía");
-            return;
-        }
-        
-        System.out.println("===================================================");
-        System.out.println("CÓDIGO\tCANTIDAD\tPRECIO");
-        System.out.println("===================================================");
-        
-        for (Venta venta : lista.ventas) {
-            System.out.printf("%d\t%d\t\t%.2f\n", 
-                venta.codigoProducto, 
-                venta.cantidadVendida, 
-                venta.precioVenta);
-        }
-        System.out.println("===================================================");
-    }
-    
-    /**
-     * Totaliza la cantidad y promedia el precio de venta por cada producto
-     * @param listaEntrada Lista con las ventas individuales
-     * @param listaSalida Lista donde se almacenarán los resultados totalizados
-     */
-    public static void sumaVentas(ListaVentas listaEntrada, ListaVentas listaSalida) {
-        if (listaEntrada.ventas.isEmpty()) {
-            return;
-        }
-        
-        // Utilizamos una clase auxiliar para almacenar productos totalizados
-        class ProductoTotalizado {
-            int codigo;
-            int cantidadTotal;
-            double sumaPrecio;
-            int cantidadVentas; // Para calcular el promedio
+            nombres[i] = nombresDisponibles[random.nextInt(nombresDisponibles.length)];
             
-            ProductoTotalizado(int codigo) {
-                this.codigo = codigo;
-                this.cantidadTotal = 0;
-                this.sumaPrecio = 0;
-                this.cantidadVentas = 0;
-            }
+            datosNomina[i][SUELDO_BASICO] = 1000000 + random.nextInt(3000001);
+            
+            double porcentajeDeduccion = 0.1 + (random.nextDouble() * 0.1);
+            datosNomina[i][DEDUCCIONES] = Math.round(datosNomina[i][SUELDO_BASICO] * porcentajeDeduccion);
+            
+            datosNomina[i][NETO_PAGAR] = datosNomina[i][SUELDO_BASICO] - datosNomina[i][DEDUCCIONES];
         }
-        
-        // Crear un arreglo temporal para almacenar los productos totalizados
-        ProductoTotalizado[] productosTotalizados = new ProductoTotalizado[1000]; // Tamaño grande para asegurar espacio
-        int contadorProductos = 0;
-        
-        // Recorrer la lista de entrada
-        for (Venta venta : listaEntrada.ventas) {
-            // Buscar si el producto ya existe en nuestro arreglo de totalizados
-            boolean encontrado = false;
-            for (int i = 0; i < contadorProductos; i++) {
-                if (productosTotalizados[i].codigo == venta.codigoProducto) {
-                    // Producto encontrado, actualizar datos
-                    productosTotalizados[i].cantidadTotal += venta.cantidadVendida;
-                    productosTotalizados[i].sumaPrecio += venta.precioVenta;
-                    productosTotalizados[i].cantidadVentas++;
-                    encontrado = true;
-                    break;
+    }
+    
+    public void ordenarPorIdentificacion() {
+        for (int i = 0; i < cantidadEmpleados - 1; i++) {
+            int minIndex = i;
+            
+            for (int j = i + 1; j < cantidadEmpleados; j++) {
+                if (identificaciones[j] < identificaciones[minIndex]) {
+                    minIndex = j;
                 }
             }
             
-            // Si no se encontró, agregar nuevo producto al arreglo
-            if (!encontrado) {
-                ProductoTotalizado nuevoProducto = new ProductoTotalizado(venta.codigoProducto);
-                nuevoProducto.cantidadTotal = venta.cantidadVendida;
-                nuevoProducto.sumaPrecio = venta.precioVenta;
-                nuevoProducto.cantidadVentas = 1;
-                productosTotalizados[contadorProductos] = nuevoProducto;
-                contadorProductos++;
+            // Intercambiar elementos si se encontró un mínimo diferente
+            if (minIndex != i) {
+                // Intercambiar identificaciones
+                int tempId = identificaciones[i];
+                identificaciones[i] = identificaciones[minIndex];
+                identificaciones[minIndex] = tempId;
+                
+                // Intercambiar nombres
+                String tempNombre = nombres[i];
+                nombres[i] = nombres[minIndex];
+                nombres[minIndex] = tempNombre;
+                
+                // Intercambiar datos de nómina
+                for (int k = 0; k < 3; k++) {
+                    double tempDato = datosNomina[i][k];
+                    datosNomina[i][k] = datosNomina[minIndex][k];
+                    datosNomina[minIndex][k] = tempDato;
+                }
             }
-        }
-        
-        // Agregar los productos totalizados a la lista de salida
-        for (int i = 0; i < contadorProductos; i++) {
-            ProductoTotalizado pt = productosTotalizados[i];
-            double precioPromedio = pt.sumaPrecio / pt.cantidadVentas;
-            // Redondear a 2 decimales
-            precioPromedio = Math.round(precioPromedio * 100.0) / 100.0;
-            
-            listaSalida.agregarVenta(pt.codigo, pt.cantidadTotal, precioPromedio);
         }
     }
     
-    /**
-     * Función principal para probar el TDA
-     */
+    public int buscarEmpleado(int identificacion) {
+        int inicio = 0;
+        int fin = cantidadEmpleados - 1;
+        
+        while (inicio <= fin) {
+            int medio = inicio + (fin - inicio) / 2;
+            
+            // Si la identificación está en el medio
+            if (identificaciones[medio] == identificacion) {
+                return medio;
+            }
+            
+            // Si la identificación es mayor, buscar en la mitad derecha
+            if (identificaciones[medio] < identificacion) {
+                inicio = medio + 1;
+            } else {
+                // Si la identificación es menor, buscar en la mitad izquierda
+                fin = medio - 1;
+            }
+        }
+        
+        return -1;
+    }
+    
+    // Función para ordenar por nombre usando QuickSort
+    public void ordenarPorNombre() {
+        quickSort(0, cantidadEmpleados - 1);
+    }
+    
+    // Implementación del algoritmo QuickSort
+    private void quickSort(int inicio, int fin) {
+        if (inicio < fin) {
+            int indicePivote = particion(inicio, fin);
+            
+            // Ordenar recursivamente los elementos antes y después del pivote
+            quickSort(inicio, indicePivote - 1);
+            quickSort(indicePivote + 1, fin);
+        }
+    }
+    
+    private int particion(int inicio, int fin) {
+        // Seleccionar el elemento del extremo derecho como pivote
+        String pivote = nombres[fin];
+        int i = inicio - 1;
+        
+        for (int j = inicio; j < fin; j++) {
+            // Si el elemento actual es menor o igual que el pivote
+            if (nombres[j].compareTo(pivote) <= 0) {
+                i++;
+                
+                // Intercambiar elementos
+                intercambiar(i, j);
+            }
+        }
+        
+        // Intercambiar el pivote con el elemento en la posición (i + 1)
+        intercambiar(i + 1, fin);
+        
+        return i + 1;
+    }
+    
+    private void intercambiar(int i, int j) {
+        // Intercambiar identificaciones
+        int tempId = identificaciones[i];
+        identificaciones[i] = identificaciones[j];
+        identificaciones[j] = tempId;
+        
+        // Intercambiar nombres
+        String tempNombre = nombres[i];
+        nombres[i] = nombres[j];
+        nombres[j] = tempNombre;
+        
+        // Intercambiar datos de nómina
+        for (int k = 0; k < 3; k++) {
+            double tempDato = datosNomina[i][k];
+            datosNomina[i][k] = datosNomina[j][k];
+            datosNomina[j][k] = tempDato;
+        }
+    }
+    
+    public void mostrarDatos() {
+        System.out.println("\n=== NÓMINA DE EMPLEADOS (ORDENADOS POR NOMBRE) ===");
+        System.out.printf("%-12s %-15s %-15s %-15s %-15s\n", 
+                        "ID", "NOMBRE", "SUELDO BÁSICO", "DEDUCCIONES", "NETO A PAGAR");
+        System.out.println("----------------------------------------------------------------");
+        
+        for (int i = 0; i < cantidadEmpleados; i++) {
+            System.out.printf("%-12d %-15s $%-14.2f $%-14.2f $%-14.2f\n", 
+                            identificaciones[i], 
+                            nombres[i], 
+                            datosNomina[i][SUELDO_BASICO], 
+                            datosNomina[i][DEDUCCIONES], 
+                            datosNomina[i][NETO_PAGAR]);
+        }
+    }
+    
+    public void mostrarEmpleado(int posicion) {
+        if (posicion >= 0 && posicion < cantidadEmpleados) {
+            System.out.println("\n=== DATOS DEL EMPLEADO ===");
+            System.out.println("Identificación: " + identificaciones[posicion]);
+            System.out.println("Nombre: " + nombres[posicion]);
+            System.out.printf("Sueldo Básico: $%.2f\n", datosNomina[posicion][SUELDO_BASICO]);
+            System.out.printf("Deducciones: $%.2f\n", datosNomina[posicion][DEDUCCIONES]);
+            System.out.printf("Neto a Pagar: $%.2f\n", datosNomina[posicion][NETO_PAGAR]);
+        } else {
+            System.out.println("Empleado no encontrado");
+        }
+    }
+    
     public static void main(String[] args) {
-        // Declarar las listas de entrada y salida
-        ListaVentas listaEntrada = new ListaVentas();
-        ListaVentas listaSalida = new ListaVentas();
+        Main nomina = new Main();
         
-        // Poblar la lista de entrada con datos aleatorios
-        poblarDatos(listaEntrada);
+        nomina.poblarDatos();
         
-        // Mostrar los datos de la lista de entrada
-        System.out.println("\nLISTA DE VENTAS ORIGINALES:");
-        mostrarDatos(listaEntrada);
+        System.out.println("DATOS INICIALES:");
+        nomina.mostrarDatos();
         
-        // Totalizar las ventas
-        sumaVentas(listaEntrada, listaSalida);
+        nomina.ordenarPorIdentificacion();
+        System.out.println("\nDATOS ORDENADOS POR IDENTIFICACIÓN:");
+        nomina.mostrarDatos();
         
-        // Mostrar los datos de la lista de salida (ventas totalizadas)
-        System.out.println("\nLISTA DE VENTAS TOTALIZADAS:");
-        mostrarDatos(listaSalida);
+        int idBuscar = nomina.identificaciones[3]; 
+        System.out.println("\nBuscando empleado con ID: " + idBuscar);
+        int posEmpleado = nomina.buscarEmpleado(idBuscar);
+        nomina.mostrarEmpleado(posEmpleado);
+        
+        int idNoExiste = 9999;
+        System.out.println("\nBuscando empleado con ID: " + idNoExiste);
+        posEmpleado = nomina.buscarEmpleado(idNoExiste);
+        if (posEmpleado == -1) {
+            System.out.println("Empleado no encontrado");
+        } else {
+            nomina.mostrarEmpleado(posEmpleado);
+        }
+        
+        nomina.ordenarPorNombre();
+        
+        System.out.println("\nDATOS ORDENADOS POR NOMBRE:");
+        nomina.mostrarDatos();
     }
 }
